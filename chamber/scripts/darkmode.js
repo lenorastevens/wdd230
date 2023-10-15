c0bbbd64ca4f930eae3fd3e54cfc3d31e79cf2fb
lenorastevens <114180320+lenorastevens@users.noreply.github.com>
2023-10-14 22:28:44 -0500

    dark mode tweaks

--- chamber/scripts/darkmode.js
+++ chamber/scripts/darkmode.js
@@ -1,52 +1,21 @@
-const modeButton = document.querySelector("#mode");
-const body = document.querySelector("body");
-const darkH1 = document.querySelector("#head1D");
-const darkH2 = document.querySelector("#head2D");
-const darkH3 = document.querySelector("#head3D");
-const header = document.querySelector("#title");
-const darkT = document.querySelector(".items");
-const darkF = document.querySelector("footer");
-const darkP1 = document.querySelector("#address");
-const darkP2 = document.querySelector("#name");
-const darkP3 = document.querySelector("#maps");
-const darkP4 = document.querySelector("#contact");
-const darkP5 = document.querySelector("#lastModified");
+function darkMode() {
+    const body = document.body;
+    const darkT = document.querySelector(".items");
+    const darkHT = document.querySelector("#title");
+    const darkF = document.querySelector("footer");
+    const darkH1 = document.querySelector("#head1D");
+    const darkH2 = document.querySelector("#head2D");
+    const darkH3 = document.querySelector("#head3D");
+    const logoB = document.querySelector("#logo");
 
 
-modeButton.addEventListener("click", () => {
-    if (modeButton.textContent.includes("🌙")) {
-        body.style.background = "#000";
-        body.style.color = "#000";
-        modeButton.textContent = "🌞";
-        modeButton.style.background = "#4F5165";
-        darkH1.style.color = "#50d8d7";
-        darkH2.style.color = "#50d8d7";
-        darkH3.style.color = "#50d8d7";
-        header.style.color = "#50d8d7";
-        darkT.style.background = "#4F5165";
-        darkF.style.background = "#4F5165";
-        darkP1.style.color = "#50d8d7";
-        darkP2.style.color = "#50d8d7";
-        darkP3.style.color = "#50d8d7";
-        darkP4.style.color = "#50d8d7";
-        darkP5.style.color = "#50d8d7";
+    body.classList.toggle("dark-mode");
+    darkT.classList.toggle("darkHF")
+    darkHT.classList.toggle("darkHF");
+    logoB.classList.toggle("logo-dark")
+    darkH1.classList.toggle("dark-mode");
+    darkH2.classList.toggle("dark-mode");
+    darkH3.classList.toggle("dark-mode");
+    darkF.classList.toggle("darkHF");
 
-
-    } else {
-        body.style.background = "#eee";
-        body.style.color = "#000";
-        modeButton.textContent = "🌙";
-        darkH1.style.color = "#474044";
-        darkH2.style.color = "#474044";
-        darkH3.style.color = "#474044";
-        header.style.color = "#474044";
-        darkT.style.background = "#50d8d7";
-        modeButton.style.background = "#50d8d7";
-        darkF.style.background = "#50d8d7";
-        darkP1.style.color = "#474044";
-        darkP2.style.color = "#474044";
-        darkP3.style.color = "#474044";
-        darkP4.style.color = "#474044";
-        darkP5.style.color = "#474044";
-    }
-});
+}
