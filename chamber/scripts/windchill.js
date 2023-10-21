49a086e00d3c5c6722c56ac6847f2d1bb768c0b2
lenorastevens <114180320+lenorastevens@users.noreply.github.com>
2023-10-21 09:42:39 -0500

    work on windchill info section of chamber

---
+++ chamber/scripts/windchill.js
@@ -0,0 +1,26 @@
+const tempInput = document.querySelector("#temp");
+const speedInput = document.querySelector("#speed");
+const windChillElement = document.querySelector("#windChill");
+
+function calculateWindChill() {
+    console.log("Function called");
+    const temperature = parseFloat(tempInput.value);
+    const windSpeed = parseFloat(speedInput.value);
+    console.log("Temperature:", temperature);
+    console.log("Wind Speed:", windSpeed);
+
+    if (!isNaN(temperature) && !isNaN(windSpeed)) {
+        if (temperature <= 50 && windSpeed > 3.0) {
+            const windChill = 35.74 + 0.6215 * temperature - 35.75 * Math.pow(windSpeed, 0.16) + 0.4275 * temperature * Math.pow(windSpeed, 0.16);
+
+            windChillElement.textContent = `Wind Chill: ${windChill.toFixed(2)} °F`;
+
+        } else {
+            windChillElement.textContent = "Wind Chill: N/A";
+        }
+    }
+}
+
+tempInput.addEventListener("input", calculateWindChill);
+speedInput.addEventListener("input", calculateWindChill);
+
