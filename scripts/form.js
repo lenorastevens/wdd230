6a79d0ac0030f931065efff02f8e6422571f745f
lenorastevens <114180320+lenorastevens@users.noreply.github.com>
2023-11-03 00:33:37 -0500

    working on user setup form

--- scripts/form.js
+++ scripts/form.js
@@ -1,29 +1,30 @@
 const rangeValue = document.getElementById("rangevalues");
 const range = document.getElementById("r");
 
-range.addEventListener('change', displayRatingValue);
-range.addEventListener('input', displayRatingValue);
+range.addEventListener("input", displayRatingValue);
+range.addEventListener("change", displayRatingValue);
+
 
 function displayRatingValue() {
     rangeValue.innerHTML = range.value;
 }
 
-const pwd = document.querySelector("#pwd");
-const pwd2 = document.querySelector("pwd2");
+const thispwd = document.querySelector("#pwd");
+const thispwd2 = document.querySelector("pwd2");
 const message = document.querySelector("formmessage");
 
 pwd2.addEventListener("focusout", checkSame);
 
 function checkSame() {
-    if (pwd.value !== pwd2.value) {
+    if (thispwd.value !== thispwd2.value) {
         message.textContent = "❗Passwords DO NOT MATCH";
         message.style.visibility = "show";
-        pwd2.style.backgroundColor = "#fff0f3";
-        pwd2.value = "";
-        pwd2.focus();
+        thispwd2.style.backgroundColor = "#fff0f3";
+        thispwd2.value = "";
+        thispwd2.focus();
     } else {
         message.style.display = "none";
-        pwd2.style.backgroundColor = "#fff";
-        pwd2.style.color = "#000";
+        thispwd2.style.backgroundColor = "#fff";
+        thispwd2.style.color = "#000";
     }
 }
