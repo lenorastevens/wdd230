75c5e0e57faa469b2f7b38c85e4c49da97b11d2f
lenorastevens <114180320+lenorastevens@users.noreply.github.com>
2023-11-04 22:41:03 -0500

    fix jx error

--- chamber/scripts/form.js
+++ chamber/scripts/form.js
@@ -1,4 +1,4 @@
-document.getElementById("myForm").addEventListener("submit", function (event) {
+document.getElementById("joinFor").addEventListener("submit", function (event) {
     event.preventDefault(); // Prevent the form from submitting by default
 
     // Get the current timestamp in milliseconds
@@ -6,6 +6,5 @@ document.getElementById("myForm").addEventListener("submit", function (event) {
 
     // Set the timestamp value in the hidden input field
     document.getElementById("timestamp").value = timestamp;
-
 
 });
