b1a3180428740f644b92e698318623e0daed2ed4
lenorastevens <114180320+lenorastevens@users.noreply.github.com>
2023-11-17 21:13:46 -0600

    add close button on banner

--- chamber/scripts/event.js
+++ chamber/scripts/event.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function () {
     let currentDay = new Date().getDay();
-    let displayDays = [1, 2, 3];
+    let displayDays = [1, 2, 3, 5];
 
     if (displayDays.includes(currentDay)) {
         displayEventBanner();
@@ -17,7 +17,21 @@ function displayEventBanner() {
         pElement.classList.add('moving-word');
         pElement.textContent = word;
         eventBanner.appendChild(pElement);
-    })
+    });
+
+    let closeButton = document.createElement('span');
+    closeButton.classList.add('close-button');
+    closeButton.innerHTML = '&times;';
+    eventBanner.appendChild(closeButton);
+
+    closeButton.addEventListener('click', closeEventBanner);
 
     eventBanner.style.display = 'block';
+}
+
+function closeEventBanner() {
+    let eventBanner = document.getElementById('event-banner');
+    if (eventBanner) {
+        eventBanner.style.display = 'none';
+    }
 }
