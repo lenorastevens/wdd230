c2d29afeb4ecd4bf2d538e1614cb9ba569a73419
lenorastevens <114180320+lenorastevens@users.noreply.github.com>
2023-11-24 13:31:50 -0600

    weather section tweaks

--- chamber/scripts/windchill.js
+++ chamber/scripts/windchill.js
@@ -1,13 +1,30 @@
 const tempInput = document.querySelector("#temp");
 const speedInput = document.querySelector("#speed");
 const windChillElement = document.querySelector("#windChill");
+const urlWind = `https://api.openweathermap.org/data/2.5/weather?lat=30.26&lon=-97.74&appid=2e0af6a26f96c6c75cc96bb0b60984e6&units=imperial`;
 
-function calculateWindChill() {
-    console.log("Function called");
-    const temperature = parseFloat(tempInput.value);
-    const windSpeed = parseFloat(speedInput.value);
-    console.log("Temperature:", temperature);
-    console.log("Wind Speed:", windSpeed);
+// fetch current weather API
+async function apiFetchWind() {
+    try {
+        const response = await fetch(urlWind);
+        if (response.ok) {
+            const data = await response.json();
+            console.log(data);
+            calculateWindChill(data);
+        } else {
+            throw Error(await response.text());
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+// call fetch function
+apiFetchWind();
+
+function calculateWindChill(data) {
+    const temperature = data.main.temp;
+    const windSpeed = data.wind.speed;
 
     if (!isNaN(temperature) && !isNaN(windSpeed)) {
         if (temperature <= 50 && windSpeed > 3.0) {
@@ -19,8 +36,6 @@ function calculateWindChill() {
             windChillElement.textContent = "Wind Chill: N/A";
         }
     }
+    tempInput.innerHTML = `${temperature.toFixed(0)}&deg;F`;
+    speedInput.innerHTML = `${windSpeed.toFixed(0)} mph`;
 }
-
-tempInput.addEventListener("input", calculateWindChill);
-speedInput.addEventListener("input", calculateWindChill);
-
